93078cf9d9b8a43e2806a3c4aecdac529365f89f
老明 <lesliecsb94@gmail.com>
2020-06-13 01:26:29 +0800

    Minor bug fixed

--- SetupClient.sh
+++ SetupClient.sh
@@ -1,7 +1,7 @@
 latestVersion=0.33.0
 mkdir -p /tmp/frp
 wget -O /tmp/frp/frp.tar.gz https://github.com/fatedier/frp/releases/download/v${latestVersion}/frp_${latestVersion}_linux_arm.tar.gz
-tar -C /tmp/frp -zxvf frp.tar.gz
+tar -C /tmp/frp -zxvf /tmp/frp/frp.tar.gz
 sudo cp /tmp/frp/frp_${latestVersion}_linux_arm/frpc /usr/bin
 chmod +x /usr/bin/frpc
 sudo mkdir -p /etc/frp
--- SetupServer.sh
+++ SetupServer.sh
@@ -1,7 +1,7 @@
 latestVersion=0.33.0
 mkdir -p /tmp/frp
 wget -O /tmp/frp/frp.tar.gz https://github.com/fatedier/frp/releases/download/v${latestVersion}/frp_${latestVersion}_linux_amd64.tar.gz
-tar -C /tmp/frp -zxvf frp.tar.gz
+tar -C /tmp/frp -zxvf /tmp/frp/frp.tar.gz
 sudo cp /tmp/frp/frp_${latestVersion}_linux_amd64/frps /usr/bin
 chmod +x /usr/bin/frps
 sudo mkdir -p /etc/frp
